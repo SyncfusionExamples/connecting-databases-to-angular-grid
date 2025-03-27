40f70c2f3a779d8b0a6616a473788bb42822770c
JamunaSundaramSF3699 <jamuna.sundaram@syncfusion.com>
2025-03-27 20:40:44 +0530

    Updated sample.

---
+++ Binding MS SQL database using CustomAdaptor/Grid_MSSQL/grid_mssql.client/src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component, ViewChild } from '@angular/core';
+import { DataManager } from '@syncfusion/ej2-data';
+import { GridComponent, EditSettingsModel, ToolbarItems } from '@syncfusion/ej2-angular-grids';
+import { CustomAdaptor } from './CustomAdaptor'
+import { EditService, FilterService, GridModule, PageService, GroupService, AggregateService, SortService, ToolbarService } from '@syncfusion/ej2-angular-grids';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: true,
+  providers: [EditService, ToolbarService, AggregateService, GroupService, FilterService, SortService, PageService],
+  imports: [GridModule],
+})
+export class AppComponent {
+  @ViewChild('grid') public grid?: GridComponent;
+  public data?: DataManager;
+  public editSettings?: EditSettingsModel;
+  public toolbar?: ToolbarItems[];
+  public employeeIDRules?: Object;
+  public customerIDRules?: Object;
+  public freightRules?: Object;
+  public shipCityRules?: Object;
+
+  public ngOnInit(): void {
+    this.data = new DataManager({
+      url: 'https://localhost:7242/api/grid',
+      insertUrl: 'https://localhost:7242/api/grid/Insert',
+      updateUrl: 'https://localhost:7242/api/grid/Update',
+      removeUrl: 'https://localhost:7242/api/grid/Remove',
+      // Enable batch URL when batch editing is enabled.
+      //batchUrl: 'https://localhost:7242/api/grid/BatchUpdate',
+      adaptor: new CustomAdaptor()
+    });
+    this.employeeIDRules = { required: true, number: true };
+    this.customerIDRules = { required: true };
+    this.freightRules = { required: true, min: 1, max: 1000 };
+    this.shipCityRules = { required: true };
+    this.toolbar = ['Add', 'Update', 'Delete', 'Cancel', 'Search'];
+    this.editSettings = { allowAdding: true, allowDeleting: true, allowEditing: true, mode: 'Normal' };
+  }
+}
---
+++ Binding MS SQL database using UrlAdaptor/Grid_MSSQL/grid_mssql.client/src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component, ViewChild } from '@angular/core';
+import { DataManager, UrlAdaptor } from '@syncfusion/ej2-data';
+import { GridComponent, EditSettingsModel, ToolbarItems } from '@syncfusion/ej2-angular-grids';
+import { EditService, FilterService, GridModule, PageService, GroupService, AggregateService, SortService, ToolbarService } from '@syncfusion/ej2-angular-grids';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: true,
+  providers: [EditService, ToolbarService, AggregateService, GroupService, FilterService, SortService, PageService],
+  imports: [GridModule],
+})
+export class AppComponent {
+  @ViewChild('grid') public grid?: GridComponent;
+  public data?: DataManager;
+  public editSettings?: EditSettingsModel;
+  public toolbar?: ToolbarItems[];
+  public employeeIDRules?: Object;
+  public customerIDRules?: Object;
+  public freightRules?: Object;
+  public shipCityRules?: Object;
+
+  public ngOnInit(): void {
+    this.data = new DataManager({
+      url: 'https://localhost:7150/api/grid', // Replace your hosted link.
+      insertUrl: 'https://localhost:7150/api/grid/Insert',
+      updateUrl: 'https://localhost:7150/api/grid/Update',
+      removeUrl: 'https://localhost:7150/api/grid/Remove',
+      // Enable batch URL when batch editing is enabled.
+      //batchUrl: 'https://localhost:7150/api/grid/BatchUpdate',
+      adaptor: new UrlAdaptor()
+    });
+    this.employeeIDRules = { required: true, number: true };
+    this.customerIDRules = { required: true };
+    this.freightRules = { required: true, min: 1, max: 1000 };
+    this.shipCityRules = { required: true };
+    this.toolbar = ['Add', 'Update', 'Delete', 'Cancel', 'Search'];
+    this.editSettings = { allowAdding: true, allowDeleting: true, allowEditing: true, mode: 'Normal' };
+  }
+}
